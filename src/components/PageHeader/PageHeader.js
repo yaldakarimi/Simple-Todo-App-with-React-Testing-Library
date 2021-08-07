4f122dc271d85254867a6300142bf90993c795b7
Yalda Karimi <yalda.karimi88@gmail.com>
2021-08-07 14:38:12 +0430

    feature: implement page header

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PageHeader({ title }) {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold border-b-2 mb-5 pb-2'>{title}</h1>
+    </div>
+  );
+}
