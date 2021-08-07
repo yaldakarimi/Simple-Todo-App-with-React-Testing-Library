ba0eabd9948e8a4139bcb1b14eed4d7cc6b39ab1
Yalda Karimi <yalda.karimi88@gmail.com>
2021-08-07 14:38:47 +0430

    feature: implement followers base component and page

---
+++ src/pages/FollowersPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Followers from '../components/Followers/Followers';
+
+export default function FollowersPage() {
+  return (
+    <div className='container mx-auto mt-10 max-w-3xl '>
+      <Followers />
+    </div>
+  );
+}
