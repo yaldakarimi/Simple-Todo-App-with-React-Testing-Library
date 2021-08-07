2f1d84b03993b1fd8cc041676336216061d19a64
Yalda Karimi <yalda.karimi88@gmail.com>
2021-08-07 14:36:09 +0430

    feature: implement todo components and todo page

---
+++ src/pages/TodoPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Todo from '../components/Todo/Todo';
+
+export default function TodoPage() {
+  return (
+    <div className='container mx-auto mt-10 max-w-3xl'>
+      <Todo />
+    </div>
+  );
+}
